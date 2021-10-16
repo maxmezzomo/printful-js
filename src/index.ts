66423d713f7b0035f8f34e227dbba90394bbf9d7
Maximilian Mezzomo <maxmezzomo@gmail.com>
2021-10-16 21:55:36 +0100

    add printul API type

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-export { default as default } from './lib/main';
+import printful from './lib/main';
+export default printful;
 export { DefaultErrorResponse as Error } from './types/functions';
+export type PrintfulAPI = typeof printful;
